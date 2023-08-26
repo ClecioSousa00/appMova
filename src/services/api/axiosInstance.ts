60c2f5086ac5e7c2a8bf7f5101fc2230c90da0b4
ClecioSousa00 <cleciosouza015@gmail.com>
2023-08-26 11:45:09 -0300

    chore update expo version

--- src/services/api/axiosInstance.ts
+++ src/services/api/axiosInstance.ts
@@ -1,14 +1,12 @@
 import axios from 'axios'
-// import { TMDB_APY_KEY } from '@env'
 
-// const API_KEY = '62ddf17397d15a4dc38ea4c5178f5501'
-const API_KEY = process.env.EXPO_PUBLIC_TMDB_APY_KEY
+
 const BASE_URL = 'https://api.themoviedb.org/3'
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   params: {
-    api_key: API_KEY,
+    api_key: process.env.EXPO_PUBLIC_TMDB_APY_KEY,
     language: 'pt-BR',
   },
 })
